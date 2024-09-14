fad926cc8034e40136353e506f72b8e0a2431df3
Umaru Biango <usbiango@gmail.com>
2024-09-14 18:17:06 +0400

    changed the about copy

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-document.querySelectorAll('nav a').forEach((link) => {
+// Scroll to section
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
   link.addEventListener('click', (e) => {
 	e.preventDefault();
 	const sectionId = link.getAttribute('href');
 	const section = document.querySelector(sectionId);
 	section.scrollIntoView({ behavior: 'smooth' });
   });
-});
+})
